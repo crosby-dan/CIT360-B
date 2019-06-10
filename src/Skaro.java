9059cf45f01f15853cd685993f10075c06473a21
Dan Crosby <dcrosby2010@gmail.com>
2019-06-10 08:28:49 -0500

    Servlets project

---
+++ src/Skaro.java
@@ -0,0 +1,87 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Skaro", urlPatterns={"/Servlet"})
+public class Skaro extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+
+        String output = "<html><head></head><body><frameset>"; // s variable will be the HTML output.
+        String battleNarrative = "";  // t variable will be the narrative for the battle setup
+        String battleResults = "";  // r variable will be the battle's results
+
+        try {
+            //Determine which Dalek squadron to use.
+            String d1 = request.getParameter("d1");
+            String d2 = request.getParameter("d2");
+            String d3 = request.getParameter("d3");
+            //Determine the attack target
+            String myTarget = request.getParameter("myTarget");
+
+            if (d1!=null) {
+                output=output + "<img src='img/daleksec.jpg' height='100px' width='100px'>";
+                battleNarrative=battleNarrative + "<p>Fighting a battle with Dalek Sec";
+            }
+            else if (d2!=null) {
+                output=output + "<img src='img/dalekwarriors.jpg' height='100px' width='100px'>";
+                battleNarrative=battleNarrative + "<p>Fighting a battle with Dalek Warriors";
+            }
+            else if (d3!=null) {
+                output=output + "<img src='img/supremecouncil.jpg' height='100px' width='100px'>";
+                battleNarrative=battleNarrative + "<p>Fighting a battle with the Dalek Supreme Council";
+            }
+            else {
+                output=output+ "No Dalek armies were specified.   As a result, your attack has failed, and the late night television comics are all laughing at the Daleks.</HTML>";
+                out.println(output + "</frameset></body></html>");
+                return;
+            }
+
+            //Add the "vs" image that goes between the Dalek and the Target.
+            output=output + "<img src='img/vs.jpg' height='100px' width='100px'>";
+
+            //These lines for debugging only
+            //s=s+"<p>myTarget=" + myTarget + "</p>";
+            //s=s+"<p>Attack Destination : " + myTarget + "</p>";
+
+            if (myTarget.equals("1")) {
+                output=output + "<img src='img/dallas.jpg' height='100px' width='100px'>";
+                battleNarrative=battleNarrative + " against the city of Dallas!</p>";
+                battleResults="<H4>It's a lucky day in Dallas, Dr. Who arrived in his Tardis and foiled the Dalek's.</H4>";
+                battleResults=battleResults + "<img src='img/tardis.png' height='200px' width='100px'>";
+            }
+            else if (myTarget.equals("2")) {
+                output=output + "<img src='img/moon.jpg' height='100px' width='100px'>";
+                battleNarrative=battleNarrative + " against the city of Dallas!</p>";
+                battleResults="<H4>On finding the moon uninhabited, the Daleks became angry and destroyed the moon.</H4>";
+                battleResults=battleResults + "<img src='img/moondestroyed.jpg' height='200px' width='200px'>";
+            }
+            else if (myTarget.equals("3")) {
+                output=output + "<img src='img/byuidaho.jpg' height='100px' width='100px'>";
+                battleNarrative=battleNarrative + " against BYU Idaho!</p>";
+                battleResults="<H4>The Dalek's tried to use their ray gun, but the faith of BYU Idaho students was unstoppable.</H4>";
+                battleResults=battleResults + "<img src='img/faith.jpg' height='200px' width='200px'>";
+            }
+
+            output=output+battleNarrative + "<h2>Battle Results:</h2>" + battleResults +"</frameset></body></html>";
+            out.println(output);
+        }
+        catch (Exception ex) {
+            output="<HTML><H3>The web page that called this servlet was missing a required field.</H3><H3>As a tragic result, the entire planet was destroyed by the Dakeks.</H3></HTML>";
+            out.println(output);
+            throw new ServletException();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("This resources can only be called via form post to the Skaro servlet.");
+        throw new ServletException();
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,44 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dcros
+  Date: 5/28/2019
+  Time: 8:35 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+  <head>
+    <title>Daleks Command Central</title>
+  </head>
+  <body>
+  <H1>Dan Crosby CIT 360 Daleks Command Central</H1>
+  <H2>Featuring Apache TomCat Servlets</H2>
+
+  <BR><BR>
+    <form action="Servlet" method="post">
+      <fieldset>
+      <span>
+        <img src="img/daleksec.jpg" width="100px" height="100px" alt="Dalek Sec">
+        <input type="checkbox" id="d1" name="d1">
+        <label for="d1">Dalek Sec</label>
+      </span>
+
+      <span>
+        <img src="img/dalekwarriors.jpg" width="100px" height="100px" alt="Dalek Warriors">
+        <input type="checkbox" id="d2" name="d2">
+        <label for="d2">Dalek Warriors</label>
+      </span>
+
+      <span>
+        <img src="img/supremecouncil.jpg" width="100px" height="100px" alt="Dalek Supreme Council">
+        <input type="checkbox" id="d3" name="d3">
+        <label for="d3">Dalek Supreme Council</label>
+      </span>
+      <br><br>
+
+      <input type="submit" value="Go to battle!">
+  </fieldset>
+    </form>
+
+  </body>
+</html>
